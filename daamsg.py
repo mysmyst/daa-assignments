ef446ee5b4fba19f23b1b1b54c5508be033cc2f7
mysmyst <faizan.122000@gmail.com>
2020-10-21 04:52:26 +0000

    --

--- daamsg.py
+++ daamsg.py
@@ -1,9 +1,14 @@
 
 node=input("enter number of nodes in graph\n")
 node=int(node)
-
+stages=input("enter number of stages of graph\n")
+stages=int(stages)
 graph=[]
-x = 99999999
+x = 9999
+dest=[0]*node
+path=[0]*stages
+path[0]=0
+path[stages-1]=node-1
 
 for i in range(node-1):
     inner=[]
@@ -13,25 +18,29 @@ for i in range(node-1):
 
 for i in range(node-1):
     for j in range(i+1,node):
-        print("enter distance of "+str(i+1)+"th node to "+str(j+1)+"th node. enter x if they are not connected")
+        print("enter distance of "+str(i)+"th node to "+str(j)+"th node. enter x if they are not connected")
         inp=input()
         if inp=='x':
             graph[i][j]=x
         else:
             graph[i][j]=int(inp)
+  
+dist = [0]*node 
+dist[node-1]=0
 
-def MSG(graph,node,x):  
-
-	dist = [0]*node 
-	dist[node-1]=0
-
-	for i in range(node-2,-1,-1): 
-		dist[i]=x 
-		for j in range(node):
-			if graph[i][j]==x: 
-				continue
-			dist[i]=min(dist[i],graph[i][j]+dist[j]) 
-	return dist[0] 
+for i in range(node-2,-1,-1): 
+    dist[i]=x 
+    for j in range(node):
+        if graph[i][j]==x: 
+            continue
+        if dist[i]>=min(dist[i],graph[i][j]+dist[j]):
+            dist[i]=min(dist[i],graph[i][j]+dist[j])
+            dest[i]=j
 
 print("shortest distance from first node to final node is:")         
-print(MSG(graph,node,x)) 
+print(dist[0])
+
+print("path to sink is:")
+for i in range(1,stages-1):
+    path[i]=(dest[path[i-1]])
+print(path)
